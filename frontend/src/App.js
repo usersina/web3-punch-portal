f934b0c996a0d8c35e0b32e1a2bab32c0638bed2
usersina <benna.anis2@gmail.com>
2021-11-28 18:45:20 +0100

    Connect to wallet & invoke a contract function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,69 @@
-// import { ethers } from "ethers";
-import React, { useEffect } from 'react';
+import { ethers } from 'ethers';
+import React, { useEffect, useState } from 'react';
+import { abi } from './utils/PunchPortal.json';
 import './App.css';
+
+const CONTRACT_ADDRESS = '0xaCEd4c3c1B03948B3cA57E4047a29F8f1491f746';
 
 export default function App() {
   // https://rinkeby.etherscan.io/address/0xaCEd4c3c1B03948B3cA57E4047a29F8f1491f746
-  const punch = () => {};
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        return alert('Download MetamMsk!');
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const punch = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        return console.log('Ethereum object does not exist!');
+      }
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const punchPortalContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+
+      let count = await punchPortalContract.getTotalPunches();
+      console.log('Number of punches so far:', count.toNumber());
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const checkIfWalletIsConnected = () => {
+    const checkIfWalletIsConnected = async () => {
       const { ethereum } = window;
 
       if (!ethereum) {
         return console.log("Looks like you don't have metamask installed!");
       }
 
-      console.log('Ethereum object is here!', ethereum);
+      // Check if user allowed access to their wallet
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      if (!accounts.length > 0)
+        return console.log('No authorized account found!');
+
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
     };
     checkIfWalletIsConnected();
   }, []);
@@ -34,9 +83,15 @@ export default function App() {
           punch! Connect your Ethereum wallet and punch me!
         </div>
 
-        <button className="waveButton" onClick={punch}>
-          Punch Me!
-        </button>
+        {currentAccount ? (
+          <button className="waveButton" onClick={punch}>
+            Punch Me!
+          </button>
+        ) : (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet!
+          </button>
+        )}
       </div>
     </div>
   );
