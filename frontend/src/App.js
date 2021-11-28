b7bda63e93f89ae87fc9bffa72636d40ec3a3216
usersina <benna.anis2@gmail.com>
2021-11-28 18:20:18 +0100

    Get ethereum object & update favicons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,41 @@
-import * as React from "react";
-import { ethers } from "ethers";
+// import { ethers } from "ethers";
+import React, { useEffect } from 'react';
 import './App.css';
 
 export default function App() {
+  // https://rinkeby.etherscan.io/address/0xaCEd4c3c1B03948B3cA57E4047a29F8f1491f746
+  const punch = () => {};
 
-  const wave = () => {
-    
-  }
-  
+  useEffect(() => {
+    const checkIfWalletIsConnected = () => {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        return console.log("Looks like you don't have metamask installed!");
+      }
+
+      console.log('Ethereum object is here!', ethereum);
+    };
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          <span role="img" aria-label="icon">
+            👊
+          </span>
+          Greetings, fighter!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am the best fighter around and I'm not afraid of taking your best
+          punch! Connect your Ethereum wallet and punch me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
+        <button className="waveButton" onClick={punch}>
+          Punch Me!
         </button>
       </div>
     </div>
